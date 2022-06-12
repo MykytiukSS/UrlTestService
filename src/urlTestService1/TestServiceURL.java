343c835dddc43054a3b7bcc3bbb254a68289fad9
MykytiukSS <m.sergeysergeyevich@gmail.com>
2022-06-12 08:47:35 +0300

    done and tested

--- src/urlTestService1/TestServiceURL.java
+++ src/urlTestService1/TestServiceURL.java
@@ -2,42 +2,71 @@ package urlTestService1;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
+
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.net.ssl.HttpsURLConnection;
-
 public class TestServiceURL {
-     
-     
-     
-     public List<String>tested(ArrayList<String>list)throws IOException{
-    	List<String>temp=new ArrayList<>();
-    	for (String string : list) {
-    		boolean test=false;
-    		try {
+//   This method returns list of correct URLs, ignoring incorrect URLs
+	public List<String> testedURL(List<String> listUrl) throws IOException {
+		List<String> temp = new ArrayList<>();
+		for (String string : listUrl) {
+			boolean test = false;
+			try {
 				URL url = new URL(string);
-				HttpURLConnection connection = (HttpURLConnection)url.openConnection();
-				int x =connection.getResponseCode();
-				if(x==200) {
-					test=true;
+				HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+				int x = connection.getResponseCode();
+				if (x == 200) {
+					test = true;
 				}
-				
-			} catch (MalformedURLException e) {
-				
+
+			} catch (Exception e) {
+
 				continue;
 			}
-    		if(test=true) {
-    			temp.add(string);
-    		}
-				
-			
-			
+			if (test = true) {
+				temp.add(string);
+			}
+
 		}
-    	 
-    	 
-    	 return temp; 
-     }
+
+		return temp;
+	}
+	//This method only shows lists  of correct and incorrect URLs
+	public void showCorrectUrl(List<String> listUrl) {
+		List<String> tempCorrect = new ArrayList<>();
+		List<String> tempIncorrect = new ArrayList<>();
+		
+		for (String string : listUrl) {
+			boolean test = false;
+			try {
+				URL url = new URL(string);
+				HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+				int x = connection.getResponseCode();
+				if (x == 200) {
+					test = true;
+				}
+
+			} catch (Exception e) {
+                 tempIncorrect.add(string);
+                 continue;
+				
+			}
+			if (test = true) {
+				tempCorrect.add(string);
+			}
+
+		}
+		System.out.println("These URLs are correct:");
+		for(String string : tempCorrect) {
+	    	 System.out.println(string);
+		}
+		System.out.println();
+		System.out.println("These URLs are incorrect:");
+		for(String string : tempIncorrect) {
+	    	 System.out.println(string);
+		}
+		
+	}
 }
