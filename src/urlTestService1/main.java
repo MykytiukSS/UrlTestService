4902296165b37bedb6f6e333de66360de55cebff
Serhii Mykytiuk <99288770+MykytiukSS@users.noreply.github.com>
2022-06-12 09:22:37 +0300

    Update main.java

--- src/urlTestService1/main.java
+++ src/urlTestService1/main.java
@@ -7,6 +7,8 @@ public class Main {
 		List<String>listUrl = new ArrayList(Arrays.asList("https://pureinfotech.com/credential-manager-windows-10","https://q.prog.kiev.ua/login","https://rozetka.com.ua/telefony-tv-i-ehlektronika/c4627949/","https://ru.wikipedia.org/wiki/%D0%A0%D0%BE%D0%B7%D0%B5%D1%82%D0%BA%D0%B0","https://danilovs.eu/","https://www.msn.com/en-xl/europe/top-stories/chemical-plant-hit-in-non-stop-ukraine-fighting/ar-AAYmxiw?ocid=msedgntp&cvid=ac527b2953ce43218fea86e2a714c706","https://www.google.co.uk/","https://www.googleeeeee.co.uk/","https://torgzona.net.ua/","https://torgggzonnna.net.ua/"
 ));
 		TestServiceURL list = new TestServiceURL();
+			
+			
 		//for getting list of correct URLs remove comment below
 		
 		
@@ -17,9 +19,7 @@ public class Main {
 		
 		list.showCorrectUrl(listUrl);
 		
-	}
-
-		
+	}		
 
 	}
 
