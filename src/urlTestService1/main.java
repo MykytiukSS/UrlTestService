2c85bfa2a827400788b9f40bf4f9b9db6096f0ca
Serhii Mykytiuk <99288770+MykytiukSS@users.noreply.github.com>
2022-06-12 09:24:15 +0300

    Update main.java

--- src/urlTestService1/main.java
+++ src/urlTestService1/main.java
@@ -2,7 +2,7 @@ package urlTestService1;
 
 public class Main {
 
-	public static void main(String[] args) {
+	
 		public static void main(String[] args) throws IOException {
 		List<String>listUrl = new ArrayList(Arrays.asList("https://pureinfotech.com/credential-manager-windows-10","https://q.prog.kiev.ua/login","https://rozetka.com.ua/telefony-tv-i-ehlektronika/c4627949/","https://ru.wikipedia.org/wiki/%D0%A0%D0%BE%D0%B7%D0%B5%D1%82%D0%BA%D0%B0","https://danilovs.eu/","https://www.msn.com/en-xl/europe/top-stories/chemical-plant-hit-in-non-stop-ukraine-fighting/ar-AAYmxiw?ocid=msedgntp&cvid=ac527b2953ce43218fea86e2a714c706","https://www.google.co.uk/","https://www.googleeeeee.co.uk/","https://torgzona.net.ua/","https://torgggzonnna.net.ua/"
 ));
@@ -20,7 +20,6 @@ public class Main {
 		list.showCorrectUrl(listUrl);
 		
 	}		
-
-	}
-
 }
+
+
