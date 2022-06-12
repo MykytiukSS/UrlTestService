073be6a1f7d7a600f2eb272a4d7d4805dcb6ade2
MykytiukSS <m.sergeysergeyevich@gmail.com>
2022-06-12 07:33:32 +0300

    Added method TestedURL to class TestServiceURL

--- src/urlTestService1/TestServiceURL.java
+++ src/urlTestService1/TestServiceURL.java
@@ -1,5 +1,43 @@
 package urlTestService1;
 
-public class TestServiceURL {
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.net.ssl.HttpsURLConnection;
+
+public class TestServiceURL {
+     
+     
+     
+     public List<String>tested(ArrayList<String>list)throws IOException{
+    	List<String>temp=new ArrayList<>();
+    	for (String string : list) {
+    		boolean test=false;
+    		try {
+				URL url = new URL(string);
+				HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+				int x =connection.getResponseCode();
+				if(x==200) {
+					test=true;
+				}
+				
+			} catch (MalformedURLException e) {
+				
+				continue;
+			}
+    		if(test=true) {
+    			temp.add(string);
+    		}
+				
+			
+			
+		}
+    	 
+    	 
+    	 return temp; 
+     }
 }
--- src/urlTestService1/main.java
+++ src/urlTestService1/main.java
@@ -1,6 +1,6 @@
 package urlTestService1;
 
-public class main {
+public class Main {
 
 	public static void main(String[] args) {
 		
